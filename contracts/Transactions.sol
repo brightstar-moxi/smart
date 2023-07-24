22f496b943a3ce22d5f55b7b532ee0d9160e8710
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-24 04:23:05 +0100

    updates

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -5,7 +5,26 @@ pragma solidity ^0.8.0;
 contract Transactions {
     uint256 transactionCounter;
 
-    event Transfer(address from, address receiver, uint amount, string message, uint256 timestop, string keyword);
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
 
-    
+     struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+     }
+
+     TransferStruct[] transactions;
+
+     function addToBlockchain() public{
+
+     }
+     function getAllTransactions() public view returns (TransferStruct[] memory){
+//return transaction
+     }
+     function addToBlockchain() public{
+
+     }
 }
