9fa41066549cdbccfa613b6c92d983b31b9a726a
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-25 17:02:26 +0100

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,11 @@
-//https://eth-goerli.g.alchemy.com/v2/dslVM0L-zLC9vjYxUpQmgPrY3iebXB7-
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     goerli:{
-      url:'https://eth-goerli.g.alchemy.com/v2/dslVM0L-zLC9vjYxUpQmgPrY3iebXB7-'
+      url:'https://eth-goerli.g.alchemy.com/v2/dslVM0L-zLC9vjYxUpQmgPrY3iebXB7-',
+          accounts: ['a930951c6ddd49bebb08ced13c14b38bba90d3740920d45a30bd819ffea9b530']
     }
   }
 }
