4b57524ffdecf56f4fe9dd1bc3e9933df186f3f5
brightstar-moxi <brightstarmoxiz2@gmail.com>
2023-07-25 16:50:29 +0100

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,9 @@ require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: '0.8.0',
+  networks: {
+    goerli:{
+      url:'https://eth-goerli.g.alchemy.com/v2/dslVM0L-zLC9vjYxUpQmgPrY3iebXB7-'
+    }
+  }
 }
